3fc1ada9591ab3607d920fcb9b1644fed6cd09be
Richard Emslie <richardemslie@gmail.com>
2018-06-24 18:21:54 +0100

    add reverseByte()

--- src/cpp/k273/algo.h
+++ src/cpp/k273/algo.h
@@ -33,4 +33,14 @@ namespace K273 {
         std::unordered_map <T, int> counts;
     };
 
+    // from stackoverflow :
+    // https://stackoverflow.com/questions/2602823/in-c-c-whats-the-simplest-way-to-reverse-the-order-of-bits-in-a-byte
+    inline uint8_t reverseByte(uint8_t n) {
+        static unsigned char lookup[16] = {
+            0x0, 0x8, 0x4, 0xc, 0x2, 0xa, 0x6, 0xe,
+            0x1, 0x9, 0x5, 0xd, 0x3, 0xb, 0x7, 0xf, };
+
+        return (lookup[n&0b1111] << 4) | lookup[n>>4];
+    }
+
 }
